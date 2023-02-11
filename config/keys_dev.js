66cb4ea42ee4f2c78c6099f679eaf4fc4780dcb6
Marharyta Voznenko <ritavoznenko@gmail.com>
2023-02-11 18:37:54 +0200

    first commit

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:"mongodb+srv://kingrings:Zdjhjyxbr1995@cluster0.nltaj.mongodb.net/final-project?retryWrites=true&w=majority",
+  secretOrKey: 'random very very secret string',
+};
+
+
